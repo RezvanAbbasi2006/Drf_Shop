ed5fbddc085461370a03742ff8c87bf8cc859348
RezvanAbbasi <rezvan.abasi555@gmail.com>
2023-12-07 13:53:40 +0330

    cart

--- apps/models/cart/models.py
+++ apps/models/cart/models.py
@@ -1,54 +1,48 @@
-from django.contrib.auth import get_user_model
+from __future__ import unicode_literals
+
 from django.db import models
 
-from apps.core.models import BaseModel
 from apps.models.product.models import Product
+from apps.user.models import User
 
-User = get_user_model()
 
-
-class Cart(models.Model):
-    """
-    Create shopping cart
-    """
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
-    is_active = models.BooleanField(default=True)
-    created_data = models.DateTimeField(auto_now_add=True)
+class OrderItem(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.SET_NULL, null=True)
+    is_ordered = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now=True)
+    date_ordered = models.DateTimeField(null=True)
 
     def __str__(self):
-        return self.customer.__str__()
+        return self.product.name
 
 
-class CartItem(models.Model):
-    """
-    Filling the specified cart with products
-    """
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_item')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
-    total_price = models.FloatField(max_length=50, blank=True)
-    count = models.IntegerField(default=1)
-    create = models.DateTimeField(auto_now_add=True)
+class Order(models.Model):
+    ref_code = models.CharField(max_length=15)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    is_ordered = models.BooleanField(default=False)
+    items = models.ManyToManyField(OrderItem)
+    date_ordered = models.DateTimeField(auto_now=True)
+
+    def get_cart_items(self):
+        return self.items.all()
+
+    def get_cart_total(self):
+        return sum([item.product.price for item in self.items.all()])
 
     def __str__(self):
-        return f"{self.cart.customer} - {self.product}"
-
-    def get_total_price(self):
-        return self.count * self.product.price
+        return '{0} - {1}'.format(self.owner, self.ref_code)
 
 
 class Transaction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_transaction')
-    cart_id = models.CharField(max_length=120)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=120)
-    amount = models.DecimalField(max_digits=100)
+    order_id = models.CharField(max_length=120)
+    amount = models.DecimalField(max_digits=100, decimal_places=2)
     success = models.BooleanField(default=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     def __str__(self):
-        return f'{self.cart_id} is transacting.'
+        return self.order_id
 
     class Meta:
         ordering = ['-timestamp']
-
-
-
